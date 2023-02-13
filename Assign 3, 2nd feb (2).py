4c8be1b3546cab274e9d68125be5d8642bfb12e0
rickbasu <124875381+rickbasu@users.noreply.github.com>
2023-02-14 02:13:44 +0530

    Add files via upload

---
+++ Assign 3, 2nd feb (2).py
@@ -0,0 +1,84 @@
+#Quention no 1
+"For loop: A loop is a interation method that is best when u know the no of interaction ahed of time"
+# eg :
+ n= [1,2, 3]
+ for i in n :
+    print(i)
+    
+    
+ "while loop : A while loop is a interaction method that is used when you dont know the no of interaction  ahed of time "
+# eg :
+ i= 1
+    while i < 6 
+    print(i)
+     i+ = 1
+1
+2
+3
+4
+5
+
+#Question no 2 ( using whiloe loop)
+sum_= 0
+product + 1
+i = 1
+while i <= 10:
+    sum_+ 1
+    product= product* i 
+    i+ = 1
+    print(sum)
+    print(product)
+    
+    55
+    3628800
+    
+    # Question no 3
+    user_ input= int(input("please enter your monthly unit consumed ?"))
+    bill=0
+    if user input<= 100
+    bill= user input*4.5
+    print(f"{user_ input} is your monthly income and total bill is {bill}")
+    elif user_input<= 200:
+    bill= 450 + (user_ inpiut - 100 )*6
+    print(f"{user_ input}is your monthly income and total bill is {bill}")
+    elif user_input<= 300:
+        bill= 450 + 600 + 1000 + ( user_ input-300 ) * 20
+        print(f"{user_input- 300 }is your monthly total bill { bill}")
+        
+        310 is your monthly unit and total bill is 2250
+        
+        
+        #question no 4 
+        
+        nums = [ ]
+        for num in range ( 1 , 101 ):
+            num.append(num)
+            
+            new _list=[ ]
+            for num in nums :
+                cube= num* num
+                while cube % 5 == 0 and cube % 4 == 0:
+                    new_list.append(num)
+                    break
+                    print(new_list)
+               [ 2,4, 5 , 6 , 8 , 10 , 12 , 14 , 15 , 16 , 18 , 20 ,22 ,24 , 25 ,26 , 28 , 30 , 32 , 34 , 35 , 36 , 38 , 40 , 42 , 44 ,45,46 , 48 ,50 , 52 , 54 , 56 , 58 , 60 , 62 , 64 ,66 , 68 , 70 , 72 , 74 , 76 , 78 . 80 , 82 , 84 , 86 , 88 , 90 , 92 , 94 , 95 ,96 , 98, 100]
+            
+            #Question no 5
+            
+            string = "i want to  become a datascienst "
+            count=0
+            for word in string:
+                vowel= [ "a" , "i' , "o" ,  "u", "e" ]
+                        if word.lower() in vowel:
+                        count += 1 
+                        print(count)
+                        12
+                        
+        
+    
+
+
+        
+        
+    
+    
